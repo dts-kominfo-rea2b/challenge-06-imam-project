9d20b76970d8f26cc9b7701849d6567f2de45dad
Bagas Imam <bagasimam.mw@gmail.com>
2022-06-11 07:13:09 +0700

    commit1

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,45 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+let allFile = [file1, file2, file3];
+function bacaData(fnCallback) {
+  let saveFile = [];
+
+  fs.readFile(file1, { encoding: "utf8" }, (err, data) => {
+    if (err) fnCallback(err);
+    const newWord = change(data);
+    saveFile.push(newWord);
+
+    fs.readFile(file2, { encoding: "utf8" }, (err, data) => {
+      if (err) fnCallback(err);
+      const newWord = change(data);
+      saveFile.push(newWord);
+
+      fs.readFile(file3, { encoding: "utf8" }, (err, data) => {
+        if (err) fnCallback(err);
+        const newWord = change(data);
+        saveFile.push(newWord);
+        fnCallback(null, saveFile);
+      });
+    });
+  });
+}
+
+function change(data){
+  const words = JSON.parse(data);
+  if (words.message != undefined){
+    const useWords = words.message.split(" ");
+    return useWords[useWords.length-1];
+
+  }else if (words[0].message != undefined){
+    const useWords = words[0].message.split(" ");
+    return useWords[useWords.length-1];
+    
+  }else if (words[0].data.message != undefined);{
+    const useWords = words[0].data.message.split(" ");
+    return useWords[useWords.length-1];
+  }
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
